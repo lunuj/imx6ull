f2a22beaaed5e67eb8834435f4e1f2926295e1b3
lunuj <2653050252@qq.com>
2024-09-21 09:54:46 +0000

    ✨ feat(中断模块): 使用中断开关beep
    进行GOC初始化，中断函数表初始化，中断向量表偏移设置。配置GPIO，设置为输入，注册中断函数，使能下降沿中断。

---
+++ 005-gpio-irq/project/inc/main.h
@@ -0,0 +1,14 @@
+// 头文件main.h，定义寄存器的地址
+#ifndef __MAIN_H
+#define __MAIN_H    
+
+#include "bsp_led.h"
+#include "bsp_delay.h"
+#include "bsp_clk.h"
+#include "bsp_beep.h"
+#include "bsp_key.h"
+#include "bsp_int.h"
+#include "bsp_exti.h"
+
+#endif
+
